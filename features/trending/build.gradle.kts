ab6ab14ae83329a397784ef6de37ea60c65de9c9
newton <wamitinewton@gmail.com>
2025-03-25 06:54:45 -0400

    initial project configuration

---
+++ features/trending/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.kotlin.compose)
+    id("kotlin-parcelize")
+    id("kotlinx-serialization")
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.newton.trending"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+//    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //hilt
+    implementation(Dependencies.Hilt.android)
+    ksp(Dependencies.Hilt.compiler)
+
+    //navigation
+    implementation(Dependencies.Hilt.navigation)
+    implementation(Dependencies.Navigation.compose)
+
+    //coil
+    implementation(Dependencies.Coil.compose)
+    implementation(Dependencies.Coil.network)
+
+    //extendedIcons
+    implementation(Dependencies.Material.extendedIcons)
+
+    //timber
+    implementation(Dependencies.Logging.timber)
+}
